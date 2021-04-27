8673292b0d1872878641912f696924b750cd6be9
Thiago Aquino <aquinolima.thiago@gmail.com>
2021-04-26 22:47:01 -0300

    splashScreen

---
+++ src/components/button.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { TouchableOpacity, StyleSheet, Text, TouchableOpacityProps} from 'react-native';
+import colors from '../styles/colors';
+
+
+interface ButtonProps extends TouchableOpacityProps {
+    title: string;
+}
+
+export function Button({ title, ... rest } : ButtonProps){
+    return(
+        <TouchableOpacity style={styles.button} activeOpacity={0.5} {... rest}>
+        <Text style={styles.buttonText}>
+          {title}
+        </Text>
+   </TouchableOpacity>
+    )
+}
+
+const styles = StyleSheet.create({
+    button:{
+        backgroundColor: colors.blue,
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderRadius: 16,
+        marginBottom: 15,
+        paddingHorizontal: 10,
+       height: 56,
+              
+    },
+    buttonText: {
+        color: colors.background, 
+        fontWeight: 'bold',
+        fontSize: 18
+   }
+
+   
+})
---
+++ src/screens/welcome.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { View, Text, Image, StyleSheet} from 'react-native';
+import colors from '../styles/colors'
+import { Button } from '../components/button'
+ 
+import welcomeImg from '../assets/welcome.png';
+
+export function Welcome () {
+    return(
+        <View style={styles.container}>
+            <Text style={styles.title} > 
+                Resgistre {'\n'}
+                 seu dia de forma {'\n'}
+                 fácil
+            </Text>
+
+            <Image source={welcomeImg} style={styles.image}/>
+            
+            <Text style={styles.subtitle} >
+                Esse é o seu diário pessoal onde você poderá anotar tudo e compartilhar com seu analista !
+            </Text>
+            
+           <Button title="Go!"/>
+           <Button title="Voltar"/>
+           <Button title="Gravar"  />
+         
+        
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'space-between'
+    },
+    title: {
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        color: colors.heading,
+        marginTop: 45,
+        paddingHorizontal: 10,
+        paddingTop: 15
+    },
+    subtitle: {
+        textAlign: 'center',
+        fontSize: 18,
+        paddingHorizontal: 30,
+        color: '#52665A'
+    },
+        image:{
+        width: 350,
+        height: 350 
+   }
+  
+
+   
+})
+
